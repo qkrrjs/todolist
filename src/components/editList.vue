1400c291728bfd853f4babbe964bb97668243d4a
munjimon <qkrrjs0411@naver.com>
2018-10-22 18:47:51 +0900

    EditList 수정
    EditMode 활성화 시 < >가 &lt; &gt;로 표시되는 점 수정 , 입력 관련 메소드 내려받아 사용 가능하도록 수정

--- src/components/editList.vue
+++ src/components/editList.vue
@@ -1,20 +1,22 @@
 <template>
-    <b-form v-if="MainEditFlag === true && todoss.id === id">
+    <b-form
+      v-if="MainEditFlag && todoss.id === id"
+      :class="MainEditFlag ? 'as' : 'bs'"
+      @submit="Deliverer(name,todoss.id)">
         <b-input-group>
             <b-form-input
-                class="a"
                 type="text"
                 maxlength="40"
-                v-show="MainEditFlag === true"
+                v-show="MainEditFlag"
                 :value="todoss.name"
                 v-model="name"
-                @keyup.enter.native="CheckNull(name,todoss.id)"
+                @keyup.esc.native="reset()"
             />
             <b-input-group-append>
                 <b-button
                     :pressed="false"
                     variant="success"
-                    @click="CheckNull(name,todoss.id)"
+                    @click="Deliverer(name,todoss.id)"
                 >수정</b-button>
                 <b-button @click="Toggle()">취소</b-button>
             </b-input-group-append>
@@ -36,29 +38,44 @@ export default {
   props: {
     todoss: Object,
     Edit: Function,
+    TagCheck: Function,
+    BlankCheck: Function,
     MainEditFlag: Boolean
   },
   created () {
     this.$EventBus.$on('idSend', (ClickedId, EditFlag) => {
-      console.log(`editer value is :${ClickedId}`)
       this.id = ClickedId
       this.ElEditFlag = EditFlag
     })
     this.$EventBus.$on('NameSend', (name) => {
-      this.name = name
+      this.name = this.inputcheck(name)
     })
   },
   methods: {
-    CheckNull (name, Id) {
-      if (name === null || name === '') {
-        alert('공백 입력은 불가능합니다')
+    Deliverer (name, Id) {
+      if (name === null || name === '' || this.BlankCheck(name) === 0) {
+        alert('공백만으로 이루어진 이름은 사용 불가능합니다')
+        return false
       } else {
-        this.Edit(name, Id)
+        this.Edit(this.TagCheck(name), Id)
       }
     },
     Toggle () {
       this.$emit('Cancel')
       this.$EventBus.$emit('FlagSend', this.ElEditFlag)
+    },
+    reset () {
+      this.name = null
+    },
+    inputcheck (name) {
+      // EditMode 활성화 중 input에서 <가 &lt;로 표시되는 것을 <로 표시되게 변경하는 역할
+      let ParamName = name
+      for (let i = 0; i < ParamName.length; i++) {
+        let slice = ParamName.substr(i, 4)
+        if (slice === '&lt;') ParamName = ParamName.replace('&lt;', '<')
+        if (slice === '&gt;') ParamName = ParamName.replace('&gt;', '>')
+      }
+      return ParamName
     }
   },
   computed: {
@@ -68,3 +85,11 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .as{
+    padding:0.3rem 0
+  }
+  .bs{
+    padding:0
+  }
+</style>
