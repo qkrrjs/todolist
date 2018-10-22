393606d032cc007437b1c848b14f316554c7d91e
munjimon <qkrrjs0411@naver.com>
2018-10-22 18:44:05 +0900

    메인 수정
    Todo_index -> ToDoMain로 파일명 변경,
    EditModeCancel로 해제 메소드 따로 생성,
    input 입력 메소드 따로 빼내어 Add와 Edit에 props로 전송

--- src/components/ToDoMain.vue
+++ src/components/ToDoMain.vue
@@ -1,15 +1,22 @@
 <template>
     <b-container>
         <div class="hello">
-              <h2>Todo List</h2>
+              <h2 class="TitleName">TodoList</h2>
         </div>
-        <addList :Add="Add"/>
+        <addList
+            :Add="Add"
+            :TagCheck="TagCheck"
+            :BlankCheck="BlankCheck"
+            @EditModeCancel="EditModeCancel()"
+          />
           <ul class="list-group">
             <li class="list-group-item" v-for="todoss in lists" :key="todoss.index">
                <editList
                   :todoss="todoss"
                   :Edit="Edit"
                   :MainEditFlag="EditFlag"
+                  :TagCheck="TagCheck"
+                  :BlankCheck="BlankCheck"
                   @changeMode="Change(todoss.id)"
                   @Cancel="Cancel(todoss.id)"
                 />
@@ -18,17 +25,18 @@
                   :Edit="Edit"
                   :MainEditFlag="EditFlag"
                   :Delete="Delete"
-                  :CompleteFlag="CompleteFlag"
                   @changeMode="Change(todoss.id)"
-                  @Complete="Complete(todoss.id)"
+                  @Complete="Complete(todoss.id, todoss.Complete)"
+                  @Renewal="RenewalComplete()"
                 />
             </li>
             <li class="list-group-item">
               <span style="float:left">List items : ( {{lists.length}} )</span>
-              <b-btn style="margin-left:-140px" @click="GetMoreTodo(lists.length, 5)">More(5)</b-btn>
+              <b-btn class="GetMoreBtn" @click="GetMoreTodo(lists.length, 5)">More(5)</b-btn>
               <b-btn @click="GetMoreTodo(lists.length, 20)">More(20)</b-btn>
             </li>
           </ul>
+          <p class="bottom-text">Made By Clive</p>
     </b-container>
 </template>
 
@@ -36,7 +44,7 @@
 // api call -> api result === success ? state change & Re: render : Exception handling
 import edit from './editList'
 import add from './addList'
-import item from './listItem'
+import item from './ItemList'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -44,8 +52,7 @@ export default {
   data () {
     return {
       AddCounter: 0,
-      EditFlag: false,
-      CompleteFlag: false
+      EditFlag: false
     }
   },
   components: {
@@ -61,14 +68,12 @@ export default {
   methods: {
     // Load
     FirstGetTodo () {
-      this.$store.dispatch('FirstGetTodo', this.ErrorChecker)
+      this.$store.dispatch('FirstGetTodo')
     },
     GetMoreTodo (LastId, limit) {
-      this.$store.dispatch('GetMoreTodo', {LastId: LastId, ErrorChecker: this.ErrorChecker, limit: limit})
-      if (this.EditFlag === true) {
-        this.EditFlag = !this.EditFlag
-        this.$EventBus.$emit('FlagSend', this.EditFlag)
-      }
+      this.$store.dispatch('GetMoreTodo', {LastId: LastId, limit: limit})
+      // 현재 EditMode가 활성화 중이라면 비활성화
+      this.EditModeCancel()
     },
     // Add
     Add (name) {
@@ -77,7 +82,7 @@ export default {
     },
     // Delete
     Delete (i) {
-      this.$store.dispatch('DeleteItem', {id: i, ErrorChecker: this.ErrorChecker})
+      this.$store.dispatch('DeleteItem', i)
     },
     // Edit
     Edit (name, id) {
@@ -85,16 +90,23 @@ export default {
       this.$EventBus.$emit('FlagSend', this.EditFlag)
       this.$store.dispatch('EditItem', {
         name: name,
-        id: id,
-        ErrorChecker: this.ErrorChecker
+        id: id
       })
     },
-    // Save & Cancel
-    Change (id) {
+    EditModeCancel () {
       if (this.EditFlag) {
         this.EditFlag = false
         this.$EventBus.$emit('FlagSend', this.EditFlag)
       }
+    },
+    // Complete Toggle
+    Complete (id, Complete) {
+      this.$store.dispatch('CompleteItem', {Id: id, Complete: Complete})
+    },
+    // Save & Cancel
+    Change (id) {
+      // EditMode 활성화 상태일시 종료시킨 뒤 컴포넌트에 변경된 Flag전송
+      this.EditModeCancel()
       this.EditFlag = true
       this.$EventBus.$emit('idSend', id, this.EditFlag)
     },
@@ -102,19 +114,30 @@ export default {
       this.EditFlag = false
       this.$EventBus.$emit('idSend', id, this.EditFlag)
     },
-    // Complete Todo
-    Complete (id) {
-      this.CompleteFlag = !this.CompleteFlag
+    // ( ADD , EDIT ) Input Checker
+    //
+    // Tag ->  '< >' check
+    TagCheck (name) {
+      let PassName = ``
+      const ParamName = name
+      for (let i = 0; i < ParamName.length; i++) {
+        const OnePieceName = ParamName.charAt(i)
+        OnePieceName === '<'
+          ? PassName += '&lt;'
+          : OnePieceName === '>'
+            ? PassName += '&gt;'
+            : PassName += OnePieceName
+      }
+      return PassName
     },
-    // ErrorCheck method
-    ErrorChecker (eCode) {
-      let msg = `Error Has Occurred. From `
-      eCode > 400 && eCode < 500
-        ? msg += `Client`
-        : eCode > 500
-          ? msg += `Server`
-          : msg = `???`
-      return msg
+    // Blank (space) Check
+    BlankCheck (name) {
+      let ValueChecker = 0
+      for (let i = 0; i < name.length; i++) {
+        if (name.charAt(i) !== ` `) ValueChecker++
+        else continue
+      }
+      return ValueChecker
     }
   },
   mounted () {
@@ -143,5 +166,19 @@ a {
 }
 .addform{
   margin-top:25px;
+}
+.GetMoreBtn{
+  margin-left:-130px;
+}
+.container{
+  padding:0 1.5rem
+}
+.TitleName{
+  margin-left:-20px
+}
+.bottom-text{
+  margin:20px 0;
+  margin-left:-30px;
+  bottom:0
 }
 </style>
