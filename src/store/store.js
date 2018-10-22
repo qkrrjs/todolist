7f7d9f35490a1fd9be0225b425c6ec9ecd669999
munjimon <qkrrjs0411@naver.com>
2018-10-22 18:46:39 +0900

    Store 수정
    예외처리 메소드 제거 , 따로 경고창으로 예외처리 , Complete Action 추가

--- src/store/store.js
+++ src/store/store.js
@@ -47,10 +47,11 @@ export default new Vuex.Store({
   },
   actions: {
     // LOAD TODO
-    FirstGetTodo (state, ErrorChecker) {
+    FirstGetTodo (state) {
       localStorage.setItem('AddCounter', null)
       const EndValue = localStorage.getItem('EndValue')
       let End
+      // 가져온 EndValue가 null이거나 0이라면 처음 출력 Todo는 5개로 그렇지 않다면 EndValue의 개수만큼 출력
       EndValue === null ||
       EndValue === 'null' ||
       parseInt(EndValue) === 0
@@ -61,8 +62,8 @@ export default new Vuex.Store({
           state.commit('SET_TODOS', res.data)
         })
         .catch((e) => {
-          alert(ErrorChecker(e.response.status))
-          // console.log(e.message)
+          alert(`List Loading Fail.`)
+          console.log(e.message)
         })
     },
     GetMoreTodo (state, payload) {
@@ -70,13 +71,15 @@ export default new Vuex.Store({
       const AddCounter = localStorage.getItem('AddCounter')
       const LastId = payload.LastId
       let LastIdValue
+      // AddCounter가 null이라면 LastId부터 limit의 개수만큼 리스트를 로드
+      // AddCounter가 있다면 LastId에서 그만큼 차감하여 리스트를 로드
       AddCounter === 'null'
         ? LastIdValue = LastId
         : LastIdValue = (LastId - parseInt(AddCounter))
       axios.get(`http://localhost:3001/todos?_start=${LastIdValue}&_limit=${payload.limit}`)
         .then((res) => {
           let ResTodo = res.data
-          // 리스트를 로드할때 방금 추가한 값이 중복 출력되는 걸 막기 위한 필터링 반복문
+          // 리스트를 로드할때 방금 추가한 값이 로드된 리스트 최하단에서 중복 출력되는 걸 막기 위한 필터링 반복문
           for (let i = 0; i < todos.length; i++) {
             for (let j = 0; j < ResTodo.length; j++) {
               if (todos[i].id === ResTodo[j].id) ResTodo.splice(j, 1)
@@ -86,20 +89,20 @@ export default new Vuex.Store({
           state.commit(`LOAD_TODO`, ResTodo)
         })
         .catch((e) => {
-          alert(payload.ErrorChecker(e.response.status))
-          // console.log(e.message)
+          alert(`List loading failed..`)
+          console.error(e)
         })
     },
     // ADD
     AddItem (state, payload) {
-      const todoData = { name: payload.name }
+      const todoData = { name: payload.name, complete: false }
       axios.post('http://localhost:3001/todos/', todoData)
         .then((res) => {
           state.commit('ADD_ITEM', res.data)
           localStorage.setItem('AddCounter', payload.AddCounter)
         }).catch((e) => {
-          alert(payload.ErrorChecker(e.response.status))
-          // console.error(e)
+          alert(`Todo ADD Fail`)
+          console.error(e)
         })
     },
     // EDIT
@@ -109,30 +112,33 @@ export default new Vuex.Store({
           state.commit('EDIT_NAME', {name: res.data.name, id: res.data.id})
         })
         .catch((e) => {
-          alert(payload.ErrorChecker(e.response.status))
-          // console.error(e)
+          alert(`Todo Edit Fail`)
+          console.error(e)
         })
     },
     // DELETE
-    DeleteItem (state, payload) {
-      let id = payload.id
-      axios.delete(`http://localhost:3001/todos/${id}`)
+    DeleteItem (state, Id) {
+      axios.delete(`http://localhost:3001/todos/${Id}`)
         .then(() => {
-          state.commit('DELETE_ITEM', id)
-          id < 6
+          state.commit('DELETE_ITEM', Id)
+          // Id가 1~5라면 EndValue를 지금 리스트 길이로 , 그렇지 않다면 null로 세팅
+          // ADD 할때 1씩 올리는 Addcounter는 삭제할 시 1씩 차감
+          Id < 6
             ? localStorage.setItem('EndValue', this.getters.lists.length)
             : localStorage.setItem('EndValue', null)
           localStorage.setItem('AddCounter', (localStorage.getItem('AddCounter') - 1))
         })
         .catch((e) => {
-          alert(payload.ErrorChecker(e.response.status))
-          // if (e.res) {
-          //   console.log(e)
-          // } else if (e.request) {
-          //   console.log(e.request)
-          // } else {
-          //   console.log('Error', e.message)
-          // }
+          alert(`Todo Delete Fail`)
+          console.error(e)
+        })
+    },
+    // COMPLETE
+    CompleteItem (state, payload) {
+      axios.patch(`http://localhost:3001/todos/${payload.Id}`, { Complete: !payload.Complete })
+        .catch((e) => {
+          alert(`Complete Toggle Fail`)
+          console.error(e)
         })
     }
   }
