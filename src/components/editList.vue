1f1d10e9b00b413e6647fc5a8664d39d46aa702a
munjimon <qkrrjs0411@naver.com>
2018-10-23 18:52:40 +0900

    editList수정
    부모에게서 내려받은 flag와 clickedId로 Editmode를 토글할 수 있게 변경

--- src/components/editList.vue
+++ src/components/editList.vue
@@ -1,6 +1,6 @@
 <template>
     <b-form
-      v-if="MainEditFlag && todoss.id === id"
+      v-if="MainEditFlag && todoss.id === clickedId"
       :class="MainEditFlag ? 'Edit' : 'Normal'"
       @submit="Deliverer(name,todoss.id)">
         <b-input-group>
@@ -10,16 +10,8 @@
                 v-show="MainEditFlag"
                 :value="todoss.name"
                 v-model="name"
-                @keyup.esc.native="reset()"
+                @keyup.esc.native="$emit('changeMode')"
             />
-            <b-input-group-append>
-                <b-button
-                    :pressed="false"
-                    variant="success"
-                    @click="Deliverer(name,todoss.id)"
-                >수정</b-button>
-                <b-button @click="Toggle()">취소</b-button>
-            </b-input-group-append>
         </b-input-group>
     </b-form>
 </template>
@@ -31,51 +23,18 @@ export default {
   data () {
     return {
       name: '',
-      id: 0,
-      ElEditFlag: false
+      id: 0
     }
   },
   props: {
     todoss: Object,
     Edit: Function,
-    TagCheck: Function,
-    BlankCheck: Function,
-    MainEditFlag: Boolean
-  },
-  created () {
-    this.$EventBus.$on('idSend', (ClickedId, EditFlag) => {
-      this.id = ClickedId
-      this.ElEditFlag = EditFlag
-    })
-    this.$EventBus.$on('NameSend', (name) => {
-      this.name = this.inputcheck(name)
-    })
+    MainEditFlag: Boolean,
+    clickedId: Number
   },
   methods: {
     Deliverer (name, Id) {
-      if (name === null || name === '' || this.BlankCheck(name) === 0) {
-        alert('공백만으로 이루어진 이름은 사용 불가능합니다')
-        return false
-      } else {
-        this.Edit(this.TagCheck(name), Id)
-      }
-    },
-    Toggle () {
-      this.$emit('Cancel')
-      this.$EventBus.$emit('FlagSend', this.ElEditFlag)
-    },
-    reset () {
-      this.name = null
-    },
-    inputcheck (name) {
-      // EditMode 활성화 중 input에서 <가 &lt;로 표시되는 것을 <로 표시되게 변경하는 역할
-      let ParamName = name
-      for (let i = 0; i < ParamName.length; i++) {
-        let slice = ParamName.substr(i, 4)
-        if (slice === '&lt;') ParamName = ParamName.replace('&lt;', '<')
-        if (slice === '&gt;') ParamName = ParamName.replace('&gt;', '>')
-      }
-      return ParamName
+      this.Edit(name, Id)
     }
   },
   computed: {
@@ -86,10 +45,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  .Edit{
-    padding:0.3rem 0
+  .Edit {
+    padding: 0.3rem 0;
   }
-  .Normal{
-    padding:0
+  .Normal {
+    padding: 0;
   }
 </style>
